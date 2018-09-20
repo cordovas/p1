b31674f364c41dc45b397981bc3e93d6910f6f5a
Steve <cordovas@bc.edu>
2018-09-20 17:16:18 -0400

    removed php from index.php

--- index.php
+++ index.php
@@ -15,13 +15,5 @@
 </body>
 </html>
 
-<!-- <?php
-$quotes = {
-    1 ==> “Outside is pure energy and colorless substance. All of the rest happens through the mechanism of our senses. Our eyes see just a small fraction of the light in the world. It is a trick to make a colored world, which does not exist outside of human beings. - Albert Hofmann ”,
 
-    2 ==> “The price of anything is the amount of life you exchange for it. -Henry David Thoreau”,
 
-    3 ==> “There have been so many times I have seen a man wanting to weep but instead beat his heart until it was unconscious. — Nayyirah Waheed"
-
-}
-?> -->
