f2a5c43708e094e897c41e97a0ff7796954055e0
Steve <cordovas@bc.edu>
2018-09-20 16:29:05 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Hello World</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <img src="images/0.jpeg" alt="">
+    
+</body>
+</html>
+
+<!-- <?php
+$quotes = {
+    1 ==> “Outside is pure energy and colorless substance. All of the rest happens through the mechanism of our senses. Our eyes see just a small fraction of the light in the world. It is a trick to make a colored world, which does not exist outside of human beings. - Albert Hofmann ”,
+
+    2 ==> “The price of anything is the amount of life you exchange for it. -Henry David Thoreau”,
+
+    3 ==> “There have been so many times I have seen a man wanting to weep but instead beat his heart until it was unconscious. — Nayyirah Waheed"
+
+}
+?> -->
