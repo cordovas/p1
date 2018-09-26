96bf1bba588a062d6513421d1075eaa5ea54bfc2
Steve <cordovas@bc.edu>
2018-09-26 13:28:37 -0400

    added meta tag to make full screen

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $quotes = [
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="mobile-web-app-capable" content="yes">
     <title>Hello World</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
