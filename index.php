b2c98f2006ed8f9b4d918671f0e22e2be5bcc916
Steve <cordovas@bc.edu>
2018-09-25 23:04:27 -0400

    took link to js from index.php

--- index.php
+++ index.php
@@ -18,7 +18,6 @@ $quotes = [
     <title>Hello World</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
-    <script src="main.js"></script>
 </head>
 <body>
     <h1>Steve Cordova</h1>
